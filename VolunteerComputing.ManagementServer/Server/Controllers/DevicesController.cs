48382bfcc2543bd234c3e7889a1929c404c1ab44
vvenomer <vvenomer@gmail.com>
2021-09-11 23:35:39 +0200

    Delete device data and refresh table

--- VolunteerComputing.ManagementServer/Server/Controllers/DevicesController.cs
+++ VolunteerComputing.ManagementServer/Server/Controllers/DevicesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using VolunteerComputing.ManagementServer.Server.Data;
 using VolunteerComputing.Shared.Models;
 
@@ -22,6 +23,28 @@ namespace VolunteerComputing.ManagementServer.Server.Controllers
         public ActionResult<IList<DeviceData>> GetDevices()
         {
             return dbContext.Devices.Include(d => d.DeviceStats).ToList();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDevices()
+        {
+            var packets = dbContext.Packets.Include(p => p.DeviceWorkedOnIt).Where(p => p.DeviceWorkedOnIt != null);
+            foreach (var packet in packets)
+            {
+                packet.DeviceWorkedOnIt = null;
+            }
+            dbContext.DeviceStats.RemoveRange(dbContext.DeviceStats);
+            dbContext.Devices.RemoveRange(dbContext.Devices);
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("DeleteStats")]
+        public async Task<IActionResult> DeleteStats()
+        {
+            dbContext.DeviceStats.RemoveRange(dbContext.DeviceStats);
+            await dbContext.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
